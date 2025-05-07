1c6b4e9985ecbb730cbdbd639b8bb205362385e7
Ishatgrepo <dmcomdot@gmail.com>
2025-05-07 14:25:04 +0530

    megadel

--- bot/helper/ext_utils/task_manager.py
+++ bot/helper/ext_utils/task_manager.py
@@ -34,7 +34,7 @@ async def stop_duplicate_check(listener):
 
 async def check_limits_size(listener, size, playlist=False, play_count=False):
     msgerr = None
-    max_pyt, megadl, torddl, zuzdl, leechdl, storage = (config_dict['MAX_YTPLAYLIST'], config_dict['MEGA_LIMIT'], config_dict['TORRENT_DIRECT_LIMIT'],
+    max_pyt, megadl, torddl, zuzdl, leechdl, storage = (config_dict['MAX_YTPLAYLIST'], config_dict['TORRENT_DIRECT_LIMIT'],
                                                         config_dict['ZIP_UNZIP_LIMIT'], config_dict['LEECH_LIMIT'], config_dict['STORAGE_THRESHOLD'])
     if config_dict['PREMIUM_MODE'] and not is_premium_user(listener.user_id):
         mdl = torddl = zuzdl = leechdl = config_dict['NONPREMIUM_LIMIT']
