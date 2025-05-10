365e6ccdb048e9be324ecdd403591c6b696017cf
Ishatgrepo <dmcomdot@gmail.com>
2025-05-10 07:33:50 +0000

    2n \d try fix

--- bot/helper/mirror_utils/status_utils/mega_download_status.py
+++ bot/helper/mirror_utils/status_utils/mega_download_status.py
@@ -1,6 +1,6 @@
 from time import time
-from bot import LOGGER, MirrorStatus
-from bot.helper.ext_utils.status_utils import get_readable_file_size, get_readable_time
+from bot import LOGGER
+from bot.helper.ext_utils.status_utils import get_readable_file_size, get_readable_time, MirrorStatus
 
 class MegaDownloadStatus:
     def __init__(self, name, size, gid, mega_listener, listener, download_path):
